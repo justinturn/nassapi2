14fd70fd799ec2db03a9c345b08bb8d6176f1281
justinturn <justinturn@gmail.com>
2017-08-22 10:34:04 -0500

    updates to include passing parms throught the api url

--- base.py
+++ base.py
@@ -5,19 +5,15 @@ state1 = 'OK'
 state2 = 'TX'
 state3 = 'NE'
 
-#stateabbrev1 = (state1 + ', ' + state2 + ', ' + state3)
-#stateabbrev = "".join(stateabbrev1)
-#print (stateabbrev)
-
 commodity = 'WHEAT'
 
 
 API_BASE = 'http://quickstats.nass.usda.gov/api/api_GET?'
 API_PAYLOAD = {'key': 'C2352E9E-5773-358B-9216-4CABA0734896',
-               'commodity_desc': ('WHEAT', 'CORN' ) ,
+               'commodity_desc': (commodity) ,
                'state_alpha': ( state1 , state2 , state3) ,
                'year': '2017',
-               'short_desc': ( 'WHEAT - ACRES HARVESTED' , 'CORN - ACRES HARVESTED' )
+               'short_desc': ( 'WHEAT - ACRES HARVESTED' )
               }
 
 apidata = requests.get(API_BASE, params=API_PAYLOAD)
