96d8cd07839b76165a3a52062a67e811384f5c76
justinturn <justinturn@gmail.com>
2017-08-23 13:54:20 -0500

    basic changes

--- base.py
+++ base.py
@@ -1,26 +1,33 @@
 import requests
 
-
+#inputs here#
 state1 = 'OK'
 state2 = 'TX'
 state3 = 'NE'
 
 commodity = 'WHEAT'
+subcommodity = 'WHEAT - ACRES HARVESTED'
 
 
+#foundation of the api here#
 API_BASE = 'http://quickstats.nass.usda.gov/api/api_GET?'
 API_PAYLOAD = {'key': 'C2352E9E-5773-358B-9216-4CABA0734896',
                'commodity_desc': (commodity) ,
                'state_alpha': ( state1 , state2 , state3) ,
                'year': '2017',
-               'short_desc': ( 'WHEAT - ACRES HARVESTED' )
+               'short_desc': ( subcommodity ) #could remove
               }
 
 apidata = requests.get(API_BASE, params=API_PAYLOAD)
 apidata = apidata.json()
 
+
+###Output Printing
+
 for info in apidata['data']:
     print(info['year'],
           info['state_name'],
           info['short_desc'],
-          info['Value'])
+          info['Value'],
+          info ['unit_desc']
+          )
